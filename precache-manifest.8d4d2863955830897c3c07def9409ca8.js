bb9e71d120e3a5ef4bbf39b588b4bf7d50f416f0
olencha <chazova.olenk@gmail.com>
2019-02-11 17:57:00 +0200

    Updates

---
+++ precache-manifest.8d4d2863955830897c3c07def9409ca8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "05fe5ed2ae8ac2dc7d03",
+    "url": "/react-weather-app/static/css/main.8eeb2163.chunk.css"
+  },
+  {
+    "revision": "05fe5ed2ae8ac2dc7d03",
+    "url": "/react-weather-app/static/js/main.05fe5ed2.chunk.js"
+  },
+  {
+    "revision": "4c4cfa3beddf0241a8a8",
+    "url": "/react-weather-app/static/js/runtime~main.4c4cfa3b.js"
+  },
+  {
+    "revision": "c4968dea8b395c10244e",
+    "url": "/react-weather-app/static/js/2.c4968dea.chunk.js"
+  },
+  {
+    "revision": "7436b193c8a4e3874730eb02822642c0",
+    "url": "/react-weather-app/static/media/drizzle.7436b193.png"
+  },
+  {
+    "revision": "51b38da820d2a1a14de171fbf5691ce7",
+    "url": "/react-weather-app/static/media/rain-d.51b38da8.png"
+  },
+  {
+    "revision": "64473a6dc4215dafbc173f66b1b5a563",
+    "url": "/react-weather-app/static/media/snow.64473a6d.png"
+  },
+  {
+    "revision": "5671dc69220f260b697174a7eaa2568e",
+    "url": "/react-weather-app/static/media/clear-d.5671dc69.png"
+  },
+  {
+    "revision": "b92a08fea9e67cad6271fbac806c5451",
+    "url": "/react-weather-app/static/media/clouds-d.b92a08fe.png"
+  },
+  {
+    "revision": "0b39c8a4aaff57398efce2c035513f6b",
+    "url": "/react-weather-app/static/media/mist.0b39c8a4.png"
+  },
+  {
+    "revision": "abb83473df1f7c5170cec647f729a16f",
+    "url": "/react-weather-app/index.html"
+  }
+];
